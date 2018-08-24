1d58afb93c8656efc30e5729f5efdeacd1ba61f8
Fábio Morais <fabiouds@hotmail.com>
2018-08-24 22:51:38 +0100

    update

--- PROG2/MINI-TESTE1/mini1/prob1.c
+++ PROG2/MINI-TESTE1/mini1/prob1.c
@@ -1,91 +1,101 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "vetor.h"
 #include "lista.h"
-#include "fila.h"
 
 /****************************************************/
 /*              Funcoes a implementar               */
 /****************************************************/
 
 /*** problema 1.1 ***/
-lista *encontra_nomes(lista *lst1, lista *lst2)
+float comprimento_medio(lista *lst)
 {
-	/* complexidade do algoritmo: ___N^2___ */
-	// iguais nas 2 listas
-	int i, j;
-	lista *aux;
-	aux = lista_nova();
-	l_elemento *lista1 = lista_inicio(lst1);
-	l_elemento *lista2;
-	for (i = 0; i < lista_tamanho(lst1); i++)
-	{
-		lista2 = lista_inicio(lst2);
-		for (j = 0; j < lista_tamanho(lst2); j++)
-		{
-			if (strcmp(lista1->str, lista2->str) == 0)
-				lista_insere(aux, lista1->str, NULL);
-			lista2 = lista2->proximo;
-		}
-		lista1 = lista1->proximo;
-	}
+	int i;
+	float x = 0.0, resultad;
+	elemento *aux;
+	aux = lista_inicio(lst);
 
-	return aux;
+	for (i = 0; i < lista_tamanho(lst); i++) 
+	{
+		x += strlen(aux->str);
+		aux = aux->proximo;
+	}
+	resultad = x / i;
+	return resultad;
 }
 
 /*** problema 1.2 ***/
-int lista_remove_longos(lista *lst, int nomes)
+lista *filtra_nomes(lista *lst, char *existe)
 {
-	//remover nomes que tenham mais palavras que nomes
-
-	int i, count, j, x = 0;
-	l_elemento *aux;
-	aux = lista_inicio(lst);
+	int i;
+	lista *aux;
+	elemento *list;
+	list = lista_inicio(lst);
+	aux = lista_nova(); 
 
 	for (i = 0; i < lista_tamanho(lst); i++)
 	{
-		count = 1;
-		for (j = 0; j < strlen(aux->str) + 1; j++)
+		if (strstr(list->str, existe) != NULL)
 		{
-			if (aux->str[j] == ' ')
-				count++;
-		}
-		if (count > nomes)
-		{
-			lista_remove(lst, aux);
+			lista_insere(aux, list->str, NULL);
+			lista_remove(lst, list);
 			i--;
-			x++;
 		}
-		aux = aux->proximo;
-	}
 
-	return x;
+		list = list->proximo;
+	}
+	return aux;
 }
 
 /*** problema 1.3 ***/
-fila *pesquisa_nomes(lista *lst, char *nome)
+const char *mais_palavras(vetor *vec)
 {
-	int i;
-	fila *aux;
-	aux = fila_nova();
-	l_elemento *lista;
-	lista= lista_inicio(lst);
-	// fila_front
-
-	for (i = 0; i < lista_tamanho(lst); i++)
+	int i, x, j, max = 0, indmax = 0;
+	elemento *aux;
+	for (i = 0; i < vetor_tamanho(vec); i++)
 	{
-
-		if(strstr(lista->str, nome))
-			fila_push(aux, lista->str);
-		lista= lista->proximo;
+		x=1;
+		for (j = 0; j < strlen(vetor_elemento(vec, i)); j++)
+		{
+			if (vec->elementos[i].str[j] == ' ')
+				x++;
+		}
+		if (x > max)
+		{
+			max = x;
+			indmax = i;
+		}
 	}
-
-	return aux;
+	return vetor_elemento(vec,indmax);
 }
 
 /****************************************************/
 /*     Funcoes ja implementadas (nao modificar)     */
 /****************************************************/
+
+vetor *lerParaVetor(FILE *ficheiro)
+{
+	char buffer[256], *nlptr;
+	vetor *v;
+
+	if (ficheiro == NULL)
+		return NULL;
+
+	v = vetor_novo();
+
+	while (!feof(ficheiro))
+	{
+		fgets(buffer, 255, ficheiro);
+		nlptr = strchr(buffer, '\n');
+		if (nlptr)
+			*nlptr = '\0';
+
+		vetor_insere(v, buffer, v->tamanho);
+	}
+
+	return v;
+}
 
 lista *lerParaLista(FILE *ficheiro)
 {
@@ -97,11 +107,13 @@ lista *lerParaLista(FILE *ficheiro)
 
 	l = lista_nova();
 
-	while (fgets(buffer, 255, ficheiro) != NULL)
+	while (!feof(ficheiro))
 	{
+		fgets(buffer, 255, ficheiro);
 		nlptr = strchr(buffer, '\n');
 		if (nlptr)
 			*nlptr = '\0';
+
 		lista_insere(l, buffer, NULL);
 	}
 
@@ -111,87 +123,58 @@ lista *lerParaLista(FILE *ficheiro)
 int main()
 {
 	FILE *f;
-	lista *l1 = NULL, *l2 = NULL, *l = NULL;
-	fila *resultado = NULL;
-	l_elemento *e;
-	int removidos;
+	vetor *v;
+	lista *l, *resultado;
+	elemento *e;
+	const char *nome;
+	int i;
+	double comp;
 
 	/* testes */
-	f = fopen("nomes1.txt", "r");
+	f = fopen("freguesias.txt", "r");
 	if (f == NULL)
 	{
 		printf("Erro ao ler ficheiro de entrada.\n");
 		return 1;
 	}
-	l1 = lerParaLista(f);
-	fclose(f);
+	l = lerParaLista(f);
 
-	f = fopen("nomes2.txt", "r");
-	if (f == NULL)
-	{
-		printf("Erro ao ler ficheiro de entrada.\n");
-		return 1;
-	}
-	l2 = lerParaLista(f);
-	fclose(f);
-
-	/* inicio teste prob1.1 */
-	l = encontra_nomes(l1, l2);
-	if (l)
-		printf("\nLista resultante contem %d nomes.\n", lista_tamanho(l));
+	/* testa comprimento_medio */
+	comp = comprimento_medio(l);
+	if (comp)
+		printf("\nComprimento medio das palavras: %.1f\n", comp);
 	else
-		printf("\nencontra_nomes retornou NULL\n");
-	/* fim teste prob1.1 */
+		printf("\ncomprimento_medio retornou 0\n");
 
-	/* inicio teste prob1.2 */
-	removidos = lista_remove_longos(l1, 4);
-	if (removidos != -1)
-	{
-		printf("\nNomes longos removidos (total %d).\n", removidos);
-		if (lista_tamanho(l1) != 224)
-			printf("Nem todos os nomes longos foram removidos da lista (tamanho: %d; esperado: 19)\n", lista_tamanho(l1));
-	}
-	else
-		printf("\nlista_remove_longos retornou ERRO\n");
-	/* fim teste prob1.2 */
+	/* testa filtra_nomes */
+	resultado = filtra_nomes(l, "Mamede");
 
-	/* inicio teste prob1.3 */
-	resultado = pesquisa_nomes(l2, "SILVA");
 	if (resultado)
 	{
-		int n = 0;
-		printf("\nNomes encontrados (%d): \n", fila_tamanho(resultado));
-		while (!fila_vazia(resultado) && n < 3)
-		{
-			printf("%s\n", fila_front(resultado));
-			fila_pop(resultado);
-			n++;
-		}
-		if (fila_tamanho(resultado) > 2)
-		{
-			puts("...");
-			while (fila_tamanho(resultado) > 1)
-				fila_pop(resultado);
-			printf("%s\n", fila_front(resultado));
-			fila_pop(resultado);
-		}
-		else
-		{
-			while (!fila_vazia(resultado))
-			{
-				printf("%s\n", fila_front(resultado));
-				fila_pop(resultado);
-			}
-		}
+		printf("\nNomes filtrados (total de %d):\n", lista_tamanho(resultado));
+		for (e = resultado->inicio; e != NULL; e = e->proximo)
+			printf("%s\n", e->str);
+
+		if (lista_tamanho(l) != 4230)
+			printf("Nem todos os nomes foram removidos do vetor (tamanho: %d; esperado: 4218)\n", lista_tamanho(l));
 	}
 	else
-		printf("\npesquisa_nomes retornou NULL\n\n");
-	/* fim teste prob1.3 */
+		printf("\nfiltra_nomes retornou NULL\n");
 
+	/* testa mais_palavras */
+	rewind(f);
+	v = lerParaVetor(f);
+	nome = mais_palavras(v);
+	if (nome)
+		printf("\nNome com mais palavras: %s\n\n", nome);
+	else
+		printf("\nmais_palavras retornou NULL\n\n");
+
+	vetor_apaga(v);
 	lista_apaga(l);
-	lista_apaga(l1);
-	lista_apaga(l2);
-	fila_apaga(resultado);
+	lista_apaga(resultado);
+
+	fclose(f);
 
 	return 0;
 }
--- PROG2/MINI-TESTE1/mini1/prob2.c
+++ PROG2/MINI-TESTE1/mini1/prob2.c
@@ -1,91 +1,79 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "pilha.h"
 
-#define BUFFER 256
+typedef struct {
+	char nome[100];
+	int pop2001;
+	int pop2012;
+}
+municipio;
 
 /****************************************************/
 /*              Funcoes a implementar               */
 /****************************************************/
 
-/*** problema 2 ***/
-float *avalia_expressoes(FILE *ficheiro, int *N)
+/*** problema 2.1 ***/
+municipio *lerFicheiro(FILE *ficheiro, int *num_municipios) 
 {
-
- char str[BUFFER];
-    pilha* p;
-    float *v=NULL;
-    p=pilha_nova();
-    (*N)=0;
-    int i,n=0,z=0;
-    float a,b,c;
-
-    while(fgets(str,BUFFER,ficheiro)!=NULL){
-        for(i=0;i<strlen(str);i++){
-            if(str[i]>='0'&&str[i]<='9'){
-                pilha_push(p,atof(str+i));
-            }
-            else
-            if(str[i]=='+'||str[i]=='-'||str[i]=='*'||str[i]=='/'){
-                a=pilha_top(p);
-                pilha_pop(p);
-                b=pilha_top(p);
-                pilha_pop(p);
-                switch(str[i]){
-                    case '+': c=b+a;break;
-                    case '-': c=b-a;break;
-                    case '*': c=b*a;break;
-                    case '/': c=b/a;break;
-                }
-                pilha_push(p,c);
-            }
-            else
-            if(str[i]=='\n'){
-                v=realloc(v,(n+1)*sizeof(float));
-                v[n]=pilha_top(p);
-                n++;
-            }
-        }
+    int n=0;
+    municipio *v;
+    v=malloc(sizeof(municipio));
+    while(fscanf(ficheiro, "%s %d %d", v[n].nome, &v[n].pop2001, &v[n].pop2012 )!= EOF)
+    {
+        n++;
+        v=realloc(v, sizeof(municipio)*(n+1));
     }
-    (*N)=n;
-    pilha_apaga(p);
-	return v;
+    *num_municipios = n;
+    return v;
 }
 
-	/****************************************************/
-	/*     Funcoes ja implementadas (nao modificar)     */
-	/****************************************************/
+/*** problema 2.2 ***/
+void gerarRelatorio(FILE *ficheiro, municipio *resultados, int num_municipios)
+{
+ 
+int i, j,res, maior, maiorind, lim=500000;
+for(j=0; j<10; j++){
+	maior=0;
+	for(i=0; i<num_municipios; i++)
+	{
+		res= resultados[i].pop2001-resultados[i].pop2012;
+		if(res>maior && res<lim)
+		{
+			maior=res;
+			maiorind=i;
+		}	
+	}
+		lim=maior;
+   fprintf(ficheiro, "%s, %d\n", resultados[maiorind].nome, maior);
+   }
 
-#define BUFFER 256
+}
+
+/****************************************************/
+/*     Funcoes ja implementadas (nao modificar)     */
+/****************************************************/
 
 int main()
 {
-	FILE *fin;
-	float *resultados;
-	char expressao[BUFFER];
-	int i, n = 0;
+	FILE *fin, *fout;
+	municipio *resultados;
+	int n;
 
-	fin = fopen("expressoes.txt", "r");
-	if (fin == NULL)
-	{
-		printf("Erro ao abrir ficheiro.\n");
+	fin = fopen("municipios.txt", "r");
+	if(fin == NULL)
 		return 1;
-	}
 
-	if ((resultados = avalia_expressoes(fin, &n)) != NULL)
-	{
-		rewind(fin);
-		for (i = 0; i < n && fgets(expressao, BUFFER, fin) != NULL; i++)
-		{
-			expressao[strlen(expressao) - 1] = '\0';
-			printf("%s -> %f\n", expressao, resultados[i]);
-		}
-	}
-	else
-		printf("Funcao retornou erro.\n");
+	fout = fopen("relatorio.txt", "w");
+	if(fout == NULL)
+		return 1;
+
+	resultados = lerFicheiro(fin, &n);
+	gerarRelatorio(fout, resultados, n);
 
 	fclose(fin);
+	fclose(fout);
+	free(resultados);
 
 	return 0;
 }
